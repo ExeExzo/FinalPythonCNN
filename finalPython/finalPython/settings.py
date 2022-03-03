aad37281cbddfa1d08b3122b6fb82c91c61f0c99
Aldiyar Akhmediyev <74849372+AldyKey@users.noreply.github.com>
2022-03-04 00:33:13 +0600

    Update settings.py

--- finalPython/finalPython/settings.py
+++ finalPython/finalPython/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cnn.apps.CnnConfig'
+    'cnn.apps.CnnConfig.cnn'
 ]
 
 MIDDLEWARE = [
