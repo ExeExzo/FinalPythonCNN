3268b0ffeacffc6bdf46f61d0002c2d64ee12f77
Bekarys <50898152+maagauiya@users.noreply.github.com>
2022-03-04 02:51:39 +0600

    Update wsgi.py

--- finalPython/finalPython/wsgi.py
+++ finalPython/finalPython/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalPython.finalPython.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalPython.settings')
 
 application = get_wsgi_application()
