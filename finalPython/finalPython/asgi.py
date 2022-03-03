85d34536fc3cc0f157b8f29a7f3ab06690a22669
Bekarys <50898152+maagauiya@users.noreply.github.com>
2022-03-04 02:57:10 +0600

    Update asgi.py

--- finalPython/finalPython/asgi.py
+++ finalPython/finalPython/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalPython.finalPython.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalPython.settings')
 
 application = get_asgi_application()
